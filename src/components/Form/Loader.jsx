934977f9d299a55e40251b844ffe021acda72dbe
Nataleesha <frojlan@gmail.com>
2023-04-26 08:09:06 +0300

    loader style

--- src/components/Form/Loader.jsx
+++ src/components/Form/Loader.jsx
@@ -1,16 +1,19 @@
+import css from "components/Form/Form.module.css";
 import { Circles } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <Circles
-      height="80"
-      width="80"
-      color="#BBE38F"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-    />
+    <div className={css["loader-container"]}>
+      <Circles
+        height="80"
+        width="80"
+        color="#BBE38F"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
   );
 };
 
